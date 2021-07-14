e53d448c62722d44f20493b8b997e5451c8dd0c7
Yoppy <yoppylands@gmail.com>
2021-07-14 16:06:00 +0900

    change review

--- src/models/review.ts
+++ src/models/review.ts
@@ -119,7 +119,7 @@ reviewSchema.post('save', async function (this: ReviewDoc) {
 reviewSchema.post(/^findOneAnd/, async (document) => {
   if (document) await document.constructor.calcAverageRatings(document.product);
 });
-reviewSchema.post(/Update/, async function (doc) {
+reviewSchema.post('save', async function (doc) {
   if (doc.userIdsWhoReport.length > 0) {
     doc.hidden = true;
   }
